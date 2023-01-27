bd5a1b095a4dae89f9f28b9f88320f1b03a7faaf
Hao Jie <haojie9777@gmail.com>
2023-01-27 23:59:27 +0800

    more refactoring

--- src/services/process_response_service.py
+++ src/services/process_response_service.py
@@ -5,19 +5,19 @@ process response from openai by:
 1. removing incomplete sentences at the end
 '''
 
-terminal_punctuation = {".", "?","!"}
+terminal_punctuation = {".", "?", "!"}
+
 
 def process_response_from_openai(response):
     last_index = max(response.rfind(i) for i in terminal_punctuation)
     if not last_index:
         return response
-    if len(response) == last_index + 1: # response already ends on a terminal punctuation
+    if len(response) == last_index + 1:  # response already ends on a terminal punctuation
         return response
     else:
-        if response[last_index-1].isdigit(): # handle edge case of fullstop belonging to a list
+        # handle edge case of fullstop belonging to a list
+        if response[last_index-1].isdigit():
             edited = response[0:last_index-1]
         else:
             edited = response[0:last_index+1]
         return edited
-        
-    
